6c76b3c21a95426774684ac5071861c4ff5f4cfa
Jordan Kupillas <78525462+JordanKoop@users.noreply.github.com>
2021-02-17 10:28:40 -0800

    Feb 17 2021

--- task2.py
+++ task2.py
@@ -15,3 +15,24 @@ outputs:
 Access granted
 Access denied
 """
+
+username = ""
+
+
+
+while username != "admin":
+    username = input("What is your username?")
+    if username!= "admin":
+        print(" Access Denied")
+
+print("Correct! What is your Password?")
+
+
+password = ""
+
+while password != "12345":
+    password = input("What is your Password?")
+    if password!= "12345":
+        print("Access Denied")
+
+print("Access Granted, Welcome back Mr. Kupillas")
